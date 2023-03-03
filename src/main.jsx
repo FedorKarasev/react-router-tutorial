4f6650ad3c9dc0b31162639a21bc76eddf8fcc78
Fedor Karasev <fedorr.karasev@gmail.com>
2023-03-03 14:01:51 +0300

    finished starter course

---
+++ src/main.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Root from './routes/root';
+import ErrorPage from './error-page';
+import Contact from './routes/contact';
+import { loader as rootLoader } from './routes/root';
+import { action as rootAction } from './routes/root';
+import { loader as contactLoader } from './routes/contact';
+import './index.css';
+import EditContact from './routes/edit';
+import { action as editAction } from './routes/edit';
+import { action as destroyAction } from './routes/destroy';
+import { action as contactAction } from './routes/contact';
+import Index from './routes';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {
+        errorElement: <ErrorPage />,
+        children: [
+          {
+            index: true,
+            element: <Index />,
+          },
+          {
+            path: 'contacts/:contactId',
+            element: <Contact />,
+            loader: contactLoader,
+            action: contactAction,
+          },
+          ,
+          {
+            path: 'contacts/:contactId/edit',
+            element: <EditContact />,
+            loader: contactLoader,
+            action: editAction,
+          },
+          {
+            path: 'contacts/:contactId/destroy',
+            action: destroyAction,
+            errorElement: <div>Ooops! There was an error</div>,
+          },
+        ],
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
